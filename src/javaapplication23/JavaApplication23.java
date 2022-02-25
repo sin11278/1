6b6180c3be3c85cf3d4b58b47352e0d1ae31d83f
Lovepreet <sin11278@sheridancollege.ca>
2022-02-24 21:16:03 -0500

    I'm done!

---
+++ src/javaapplication23/JavaApplication23.java
@@ -0,0 +1,18 @@
+
+
+package javaapplication23;
+
+/**
+ * This class +++ Insert class description here +++ 
+ * @author Lovepreet Singh
+ */
+public class JavaApplication23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
